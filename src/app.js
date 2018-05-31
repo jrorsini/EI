3580a87dbcb22c582e8d1a41af20fc243896c95a
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-05-31 14:38:02 +0900

    Adding style.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import EmotionalDiary from './components/EmotionalDiary';
+import '../node_modules/normalize.css/normalize.css';
+import './styles/style.scss';
 
 ReactDOM.render(<EmotionalDiary />, document.getElementById('app'));
