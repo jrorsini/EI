502873ab82155d59d3fe1690287f5b2fad81dda7
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-06-07 13:54:08 +0900

    Created lab and now getting redux

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/app.js',
+	entry: './src/lab/redux-ei.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.join(__dirname, 'public')
