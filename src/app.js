34aa666aa7268ef7e383d729417d0a75c38590dd
Jean-Roger <jeanroger.orsini@gmail.com>
2018-06-13 05:39:19 +0900

    Added router,

--- src/app.js
+++ src/app.js
@@ -3,5 +3,18 @@ import ReactDOM from 'react-dom';
 import EmotionalDiary from './components/EmotionalDiary';
 import '../node_modules/normalize.css/normalize.css';
 import './styles/style.scss';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const EmotionalDiaryRouter = () => (
+	<Router>
+		<div>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+			</ul>
+		</div>
+	</Router>
+);
 
 ReactDOM.render(<EmotionalDiary />, document.getElementById('app'));
