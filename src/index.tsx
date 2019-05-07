56ed8ea43799ed565338f9f86fbcd906652d7208
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2019-05-07 16:43:22 +0200

    Solution

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import EntryForm from './components/EntryForm';
-
-ReactDOM.render(
-	<EntryForm firstname="Bob" lastname="Doe" />,
-	document.getElementById('app')
-);
